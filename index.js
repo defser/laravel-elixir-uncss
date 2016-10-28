99602a05dae6ef78bb7f06a5f28f5fd75e6be3ae
defser <info@defser.nl>
2016-10-28 17:59:20 +0200

    Make it work again

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
 var gulp = require('gulp');
-var cssnano = require('gulp-cssnano');
 var Elixir = require('laravel-elixir');
 var uncss = require('gulp-uncss');
 
 var config = Elixir.config;
 
-Elixir.extend('uncss', function(styles, options, output, baseDir) {
-    var paths = prepGulpPaths(styles, baseDir, output);
+Elixir.extend('uncss', function(styles, options, output) {
 
     new Elixir.Task('uncss', function() {
-        this.log(paths.src, paths.output);
+        this.log('Start uncss');
 
-        return gulp.src(paths.src.path)
-            .pipe(uncss(options || {}))
-            .pipe(Elixir.Plugins.if(config.production, cssnano()))
-            .pipe(gulp.dest(paths.output.baseDir));
+        return gulp.src(styles)
+        .pipe(uncss(options))
+        .pipe(gulp.dest(output));
     });
 });
-
-/**
- * Prep the Gulp src and output paths.
- *
- * @param  {string|array} src
- * @param  {string|null}  output
- * @return {object}
- */
-var prepGulpPaths = function(src, baseDir, output) {
-    return new Elixir.GulpPaths()
-        .src(src, baseDir || config.get('assets.css.folder'))
-        .output(output || config.get('public.css.outputFolder'), 'all.css');
-};
